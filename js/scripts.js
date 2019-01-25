ad5c2517bd60f0d1a197c000208ef60bccdfcad6
Travis Toal <travisty12@gmail.com>
2019-01-25 11:52:07 -0800

    attempt to reverse output, break code entirely

--- js/scripts.js
+++ js/scripts.js
@@ -4,11 +4,20 @@
 // up to that input number.
 function beepBoop(numIn) {
   result = "";
+  if (name == "") {
+    name = "Dave";
+  }
+  if (parseInt(numIn) == 9000) {
+    return "My God, it's full of stars."
+  } else if (parseInt(numIn) > 30) {
+    return "This mission is too important for me to allow you to jeopardize it.";
+  } else if (parseInt(numIn) <= 0) {
+    return "Stop, " + name + ". Stop, " + name + ". I am afraid. I am afraid, " + name + ".";
+  }
   for (var i = 0; i < (parseInt(numIn) + 1); i++) {
-    if (i.toString().split("").includes("3")) {
-      if (name == "") {
-        name = "Dave";
-      }
+    if (((i % 35) == 0) && (i > 0)) {
+      arrOut.push("")
+    } else if (i.toString().split("").includes("3")) {
       arrOut.push("I'm sorry " + name + ". I'm afraid I can't do that");
     } else if (i.toString().split("").includes("2")) {
       arrOut.push("Boop!");
@@ -23,7 +32,14 @@ function beepBoop(numIn) {
   return result;
 }
 
+function reverse(strIn) {
+  clear();
+  strOut = strIn.split(", ").reverse().join(", ");
+  return strOut;
+}
+
 function clear() {
+  strOut = "";
   name = "";
   result = "";
   arrOut = [];
@@ -31,6 +47,7 @@ function clear() {
   return;
 }
 
+var strOut = "";
 var name = "";
 var result = "";
 var arrOut = [];
@@ -57,16 +74,21 @@ $(document).ready(function() {
     $("#numIn").fadeOut();
     setTimeout(function(){
       $("#result").fadeIn();
-    }, 400);
-    setTimeout(function(){
+      $("#reverse").fadeIn();
       $("#restart").fadeIn();
     }, 400);
     $("#result").text(beepBoop(userIn));
     event.preventDefault();
   });
+  $("#reverse").submit(function(event) {
+    clear();
+    $("#result").text(reverse(beepBoop(userIn)));
+    event.preventDevault();
+  });
   $("#restart").click(function() {
     $("#result").fadeOut();
     $("#restart").fadeOut();
+    $("#reverse").fadeOut();
     setTimeout(function(){
       $("#start").fadeIn();
     }, 400);
