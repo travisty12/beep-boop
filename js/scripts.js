d197ff18051671633c0e1d08ae968b64d954a6d2
Travis Toal <travisty12@gmail.com>
2019-01-25 13:57:35 -0800

    add effects on certain inputs

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,9 @@ function beepBoop(numIn) {
   }
   if (parseInt(numIn) == 9000) {
     return "My God, it's full of stars."
-  } else if (parseInt(numIn) > 30) {
+  } else if (parseInt(numIn) == 35) {
+    return "I've just picked up a fault in the AE35 unit. It's going to go 100% failure in 72 hours."
+  } else if (parseInt(numIn) > 50) {
     return "This mission is too important for me to allow you to jeopardize it.";
   } else if (parseInt(numIn) <= 0) {
     return "Stop, " + name + ". Stop, " + name + ". I am afraid. I am afraid, " + name + ".";
@@ -18,6 +20,8 @@ function beepBoop(numIn) {
   for (var i = 0; i < (parseInt(numIn) + 1); i++) {
     if (((i % 35) == 0) && (i > 0)) {
       arrOut.push("")
+    } else if (((i % 7) == 0) && (i > 0)) {
+      arrOut.push("Good morning, " + name);
     } else if (i.toString().split("").includes("3")) {
       arrOut.push("I'm sorry " + name + ". I'm afraid I can't do that");
     } else if (i.toString().split("").includes("2")) {
@@ -68,6 +72,19 @@ $(document).ready(function() {
     setTimeout(function(){
       $("#numIn").fadeIn();
     }, 400);
+    if (name.includes("hal") || name.includes("HAL") || name.includes("Hal")) {
+      $("#logo").fadeOut();
+      setTimeout(function(){
+        $("#hal").fadeIn();
+      }, 400);
+    } else {
+      if ($("#hal").css("display")) {
+        $("#hal").fadeOut();
+        setTimeout(function(){
+          $("#logo").fadeIn();
+        }, 400);
+      }
+    }
     event.preventDefault();
   });
   $("#numIn").submit(function(event) {
